969a8b3c1d5485123f84c824b06f2f884fe2459f
Nicolas <nickzzz2019@gmail.com>
2025-09-04 16:53:13 -0300

    Cadastro completo

--- script.sql
+++ script.sql
@@ -3,6 +3,12 @@ CREATE DATABASE BDBiblioteca;
 -- USANDO O BANCO DE DADOS
 USE BDBiblioteca;
 
+-- LOGIN
+CREATE TABLE tbLogin(
+	codUsuario int primary key auto_increment,
+    usuario varchar(40),
+    senha varchar(40)
+);
 -- CRIANDO AS TABELAS DO BANCO DE DADOS
 CREATE TABLE tbPedidoBiblioteca(
 	codPedido int primary key auto_increment,
@@ -14,3 +20,4 @@ CREATE TABLE tbPedidoBiblioteca(
 
 -- CONSULTANDO AS TABELAS DO BANCO
 SELECT * FROM tbPedidoBiblioteca;
+SELECT * FROM tbLogin;
